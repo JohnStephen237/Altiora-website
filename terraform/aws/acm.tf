b727f00d0cde403a49a5f6f0934459172661a40a
Stephen Mbouwa <jmbouwaf@montgomerycollege.edu>
2025-09-30 20:44:15 -0400

    Let's see how this goes.

--- terraform/aws/acm.tf
+++ terraform/aws/acm.tf
@@ -1,1 +1,9 @@
+resource "aws_acm_certificate" "marketing" {
+  domain_name               = var.astro_domain_name
+  validation_method         = "DNS"
+  subject_alternative_names = ["www.${var.astro_domain_name}"]
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -1,14 +1,3 @@
-resource "aws_route53_record" "altiora_root" {
-  zone_id = data.aws_route53_zone.primary.zone_id
-  name    = var.astro_domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.marketing.domain_name
-    zone_id                = aws_cloudfront_distribution.marketing.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
 
 resource "aws_route53_record" "altiora_www" {
   zone_id = data.aws_route53_zone.primary.zone_id
